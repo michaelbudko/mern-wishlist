2dd55f3e9025fe2f90856ee4f9e989afcf3c438e
mikhailbudko <mikhail.budko@outlook.com>
2020-08-18 01:37:06 -0400

    deploying web app to heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require ("path");
 
 const items = require("./routes/api/items");
 
@@ -11,6 +12,14 @@ app.use(bodyParser.json());
 
 //routes
 app.use("/api/items", items)
+
+//production server in heroku
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "idex.html"))
+    });
+}
 
 //db config
 const db = require("./config/keys").mongoURI;
